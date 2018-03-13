29d08834c69b31ab5e65576a3947c3d9af161220
gafs01 <info@gafsad.com>
2018-03-13 11:33:36 +0100

    role and profile demo

---
+++ site/profile/lamp.pp
@@ -0,0 +1,7 @@
+class profile::lamp  {
+      include apache
+      include mysql::server
+      include php
+
+}
+
---
+++ site/profile/manifests/lamp.pp
@@ -0,0 +1,9 @@
+class profile::lamp  {
+      include apache
+      include mysql::server
+      include php
+
+}
+
+
+
